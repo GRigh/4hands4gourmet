588f1cf3cf30e776b7c30a2d97dafc1d4aa76f5b
GRigh <73525600+GRigh@users.noreply.github.com>
2025-07-13 10:15:57 +0000

    rimozione codice stupido

--- js/create_page.js
+++ js/create_page.js
@@ -1,23 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
   const path = window.location.pathname;
   const langPrefix = path.split("/")[1] ? `/${path.split("/")[1]}` : "";
-  const footerFile = "footer/footer.html";
-  let navbarFile = "navbar/navbar.html";
-  let detailFile = getDetailFile(path);
+  const footerFile = `${langPrefix}/footer/footer.html`;
+  let navbarFile = `${langPrefix}/navbar/navbar.html`;
+  let detailFile = getDetailFile(path, langPrefix);
+
+  if (path === "/" || path.endsWith("index.html")) {
+    detailFile = `${langPrefix}/navbar/detail/index.html`;
+  } else if (path.endsWith("about.html")) {
+    detailFile = `${langPrefix}/navbar/detail/about.html`;
+  } else if (path.endsWith("booking.html")) {
+    detailFile = `${langPrefix}/navbar/detail/booking.html`;
+  } else if (path.endsWith("contact.html")) {
+    detailFile = `${langPrefix}/navbar/detail/contact.html`;
+  } else if (path.endsWith("service.html")) {
+    detailFile = `${langPrefix}/navbar/detail/service.html`;
+  } else if (path.endsWith("team.html")) {
+    detailFile = `${langPrefix}/navbar/detail/team.html`;
+  } else if (path.endsWith("testimonial.html")) {
+    detailFile = `${langPrefix}/navbar/detail/testimonial.html`;
+  }
 
   // carica navbar
   fetch(navbarFile)
     .then(response => response.text())
     .then(html => {
       document.getElementById("navbar").innerHTML = html;
-      const path = window.location.pathname.split("/").pop() || "index.html";
-      //gestione link navbar
+      const currentPage = window.location.pathname.split("/").pop() || "index.html";
+      // gestione link navbar
       document
         .querySelectorAll(".navbar-nav .nav-link")
         .forEach(link => {
           const href = link.getAttribute("href");
 
-          if (href === path) {
+          if (href === currentPage) {
             link.classList.add("active");
           } else {
             link.classList.remove("active");
@@ -42,7 +58,7 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
-function getDetailFile(path) {
+function getDetailFile(path, langPrefix) {
   if (path === "/" || path.endsWith("index.html")) {
     return `${langPrefix}/navbar/detail/index.html`;
   } else if (path.endsWith("about.html")) {
