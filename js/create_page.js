7733ca223c143342efbf3b2c902f021a518b8ac5
GRigh <73525600+GRigh@users.noreply.github.com>
2025-07-15 12:13:09 +0000

    fixes

--- js/create_page.js
+++ js/create_page.js
@@ -1,32 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   const path = window.location.pathname;
   const lang = getLang(path);
+  const basePath = "/4hands4gourmet";
+
   console.log("PATH:", path);
   console.log("LANG:", lang);
-  const footerFile = `/${lang}/footer/footer.html`;
-  const navbarFile = `/${lang}/navbar/navbar.html`;
+
+  const footerFile = `${basePath}/${lang}/footer/footer.html`;
+  const navbarFile = `${basePath}/${lang}/navbar/navbar.html`;
   let detailFile = getDetailFile(path, lang);
-  console.log("DETAIL:", detailFile);
+
   // carica navbar
   fetch(navbarFile)
     .then(response => response.text())
     .then(html => {
       document.getElementById("navbar").innerHTML = html;
-      const path = window.location.pathname.split("/").pop() || "index.html";
-      //gestione link navbar
+      const page = window.location.pathname.split("/").pop() || "index.html";
+      // gestione link navbar
       document
         .querySelectorAll(".navbar-nav .nav-link")
         .forEach(link => {
           const href = link.getAttribute("href");
-
-          if (href === path) {
+          if (href === page) {
             link.classList.add("active");
           } else {
             link.classList.remove("active");
           }
         });
 
-      // solo ora il DOM della navbar esiste
       if (detailFile) {
         fetch(detailFile)
           .then(response => response.text())
@@ -52,24 +53,25 @@ function getLang(path) {
   if (segments.includes("it")) {
     return "it";
   }
-  return "it"; // fallback
+  return "it";
 }
 
 function getDetailFile(path, lang) {
+  const basePath = "/4hands4gourmet";
   if (path === "/" || path.endsWith("index.html")) {
-    return `/${lang}/navbar/detail/index.html`;
+    return `${basePath}/${lang}/navbar/detail/index.html`;
   } else if (path.endsWith("about.html")) {
-    return `/${lang}/navbar/detail/about.html`;
+    return `${basePath}/${lang}/navbar/detail/about.html`;
   } else if (path.endsWith("booking.html")) {
-    return `/${lang}/navbar/detail/booking.html`;
+    return `${basePath}/${lang}/navbar/detail/booking.html`;
   } else if (path.endsWith("contact.html")) {
-    return `/${lang}/navbar/detail/contact.html`;
+    return `${basePath}/${lang}/navbar/detail/contact.html`;
   } else if (path.endsWith("service.html")) {
-    return `/${lang}/navbar/detail/service.html`;
+    return `${basePath}/${lang}/navbar/detail/service.html`;
   } else if (path.endsWith("team.html")) {
-    return `/${lang}/navbar/detail/team.html`;
+    return `${basePath}/${lang}/navbar/detail/team.html`;
   } else if (path.endsWith("testimonial.html")) {
-    return `/${lang}/navbar/detail/testimonial.html`;
+    return `${basePath}/${lang}/navbar/detail/testimonial.html`;
   }
   return "";
 }
